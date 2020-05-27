f2179053466e802066378c64f659497b06647f1b
Trần Mạnh Hưng <48917725+TranHungKT@users.noreply.github.com>
2020-05-27 14:16:35 +0700

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,12 @@
+// REMEMBER_TO_CHANGE_YOUR_CONFIG 
+
 module.exports = {
-    apiKey: "AIzaSyAmbGF9vauOoVWe6s6hwSLsWgIR94or93g",
-    authDomain: "socialapp-73ba2.firebaseapp.com",
-    databaseURL: "https://socialapp-73ba2.firebaseio.com",
+    apiKey: "YOUR_API_KEY", 
+    authDomain: "authDomain",
+    databaseURL: "DBURL",
     projectId: "socialapp-73ba2",
-    storageBucket: "socialapp-73ba2.appspot.com",
-    messagingSenderId: "682388877105",
-    appId: "1:682388877105:web:dd6061dfddc3c4878a8a10",
-    measurementId: "G-7PLZ2V5CHR"
+    storageBucket: "storageBucket",
+    messagingSenderId: "00000000",
+    appId: "appID",
+    measurementId: "0000000"
   };
